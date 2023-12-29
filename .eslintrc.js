0d159b803252ed842a9d443660cc772cbc10538c
ForzenString <964413011@qq.com>
2023-12-29 23:58:35 +0800

    :wrench: 添加eslint 以及相关配置

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports={
+    parser: "vue-eslint-parser",
+    parserOptions: {
+        parser:"@typescript-eslint/parser",
+        sourceType: "model"
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:vue/vue3-recommended",
+    ],
+    rules: {
+
+    }
+}
