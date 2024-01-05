306a9ac3547bffa744dac6bb91afde6a4d65ba1f
ForzenString <964413011@qq.com>
2024-01-05 21:25:59 +0800

    :sparkles: 打卡模块初步实现

--- src/pages/CheckInPage.vue
+++ src/pages/CheckInPage.vue
@@ -1,9 +1,69 @@
 <template>
-    <v-card>
-        <h1>Check In Page</h1>
+  <v-container class="d-flex justify-center">
+    <face-dect
+
+      :face-detect="new FrontFaceDetectService()"
+      :face-picker="onDetectFace"
+      :height="200"
+      :width="300"
+      class="d-flex justify-center w-25 h-50 elevation-3"
+    >
+      <template #detect-activator="{activate}">
+        <v-btn
+          :disabled="!modelReady"
+          @click="activate()"
+        >
+          Check
+        </v-btn>
+      </template>
+    </face-dect>
+  </v-container>
+  <v-dialog
+    v-model="showCheckIn"
+    class="d-flex align-center"
+    location="center"
+  >
+    <v-card
+      class="w-25 d-flex align-center"
+      title="确认签到"
+    >
+      <v-card-text>
+        <img
+          ref="imageRef"
+          alt="11"
+          src=""
+        >
+      </v-card-text>
+      <v-card-actions>
+        <v-btn>Check In Confirm</v-btn>
+      </v-card-actions>
     </v-card>
+  </v-dialog>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
 
+import FaceDect from "../components/FaceDect.vue";
+import {FrontFaceDetectService} from "../service/impls/frontfaceDetect.ts";
+import {onMounted, ref} from "vue";
+
+const detectService = new FrontFaceDetectService()
+const modelReady = ref(false)
+const imageRef = ref()
+const showCheckIn = ref(false)
+const onDetectFace = (face: Blob) => {
+  const url = URL.createObjectURL(face)
+  showCheckIn.value = true
+  console.log(url)
+  setTimeout(() => {
+
+    imageRef.value.src = url
+  }, 500)
+}
+
+onMounted(() => {
+  detectService.init().then(() => {
+    modelReady.value = true
+  })
+})
 </script>
