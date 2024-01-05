71e2afdeac8ac35c65d38f7583474689990ce981
ForzenString <964413011@qq.com>
2024-01-05 20:11:09 +0800

    :bug: 修复Vuetify加载异常

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import './style.css'
 import App from './App.vue'
 import "vuetify/styles"
-import { createVuetify } from "vuetify";
-import { components, directives } from "vuetify/dist/vuetify";
+import vuetify from "./plugins/vutify.ts";
 import router from './router';
 
-const vuetify = createVuetify({ components, directives })
 createApp(App)
     .use(vuetify)
     .use(router)
