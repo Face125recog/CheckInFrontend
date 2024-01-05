42fc9680a1bbdf453c4d8f7dc3262afa2a8659ef
ForzenString <964413011@qq.com>
2024-01-05 21:25:13 +0800

    :sparkles: 添加首页路由

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,15 @@
-import { createWebHistory, createRouter } from "vue-router"
+import {createRouter, createWebHistory} from "vue-router"
 import CheckInPage from "./pages/CheckInPage.vue"
 import UserManagerPage from "./pages/UserManagerPage.vue"
+import Home from "./pages/Home.vue";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
+        {
+            path: "/",
+            component: Home
+        },
         {
             path: "/check_in",
             component: CheckInPage
