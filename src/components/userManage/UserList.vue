145aad5e23159c216cc54931c7e12235d99aae02
ForzenString <964413011@qq.com>
2024-01-05 21:26:18 +0800

    :sparkles: 添加部分管理界面

---
+++ src/components/userManage/UserList.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import {ref} from "vue";
+
+const itemPerPage = ref(5)
+const items = ref<{ name: string, identity: string }[]>([])
+const itemLength = ref(100)
+const loading = ref(false)
+
+const loadItems = (arg) => {
+  console.log(arg)
+}
+</script>
+
+<template>
+  <v-data-table-server
+    v-model:items-per-page="itemPerPage"
+  />
+</template>
+
+<style scoped>
+
+</style>
