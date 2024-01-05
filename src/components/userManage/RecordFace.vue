9b8a56ac9004021938d8f040343b36d8af79e419
ForzenString <964413011@qq.com>
2024-01-05 23:34:41 +0800

    :sparkles: 添加录入UI

--- src/components/userManage/RecordFace.vue
+++ src/components/userManage/RecordFace.vue
@@ -1,8 +1,99 @@
 <script lang="ts" setup>
 
+import FaceDect from "../FaceDect.vue";
+import {FrontFaceDetectService} from "../../service/impls/frontfaceDetect.ts";
+import {computed, onMounted, ref} from "vue";
+
+const faceDetect = new FrontFaceDetectService()
+const modelReady = ref(false)
+const remainTimes = ref(200)
+const startCollecting = ref(false)
+const onCollect = computed(() => {
+  return startCollecting.value && remainTimes.value > 0
+})
+
+const faceImg = ref()
+onMounted(() => {
+  faceDetect.init().then(() => {
+    modelReady.value = true
+  })
+})
+const activateFaceDetect = (activate: () => void) => {
+
+  activate()
+  if (onCollect.value) {
+    setTimeout(() => {
+      activateFaceDetect(activate)
+    }, 20)
+  } else {
+    startCollecting.value = false
+    remainTimes.value = 200
+  }
+
+}
+const onPickFace = (face: Blob) => {
+  faceImg.value.src = URL.createObjectURL(face)
+  remainTimes.value -= 1
+}
+
+
 </script>
 
 <template>
+  <v-dialog
+    class=""
+    transition="dialog-top-transition"
+  >
+    <template #activator="{props}">
+      <slot
+        :active="props"
+        name="activator"
+      />
+    </template>
+
+    <v-card
+      :loading="onCollect"
+      class="w-33 d-flex justify-center ma-2"
+      style="align-self: center"
+    >
+      <v-card-text class="pa-2">
+        <v-stepper
+          :items="[`登记信息`,`录入人脸`]"
+          elevation="0"
+        >
+          <template #item.1>
+            <v-card>
+              <v-card-text>
+                <v-text-field label="Name" />
+                <v-text-field label="ID" />
+              </v-card-text>
+            </v-card>
+          </template>
+          <template #item.2>
+            <face-dect
+              :face-detect="faceDetect"
+              :face-picker="onPickFace"
+              :height="200"
+              :width="300"
+              class="elevation-0 ma-2"
+            >
+              <template #detect-activator="{activate}">
+                <v-btn @click="startCollecting=true;activateFaceDetect(activate)">
+                  {{ onCollect ? `采集中 ${remainTimes + 1}/200` : '开始采集' }}
+                </v-btn>
+              </template>
+            </face-dect>
+          </template>
+        </v-stepper>
+
+        <img
+          ref="faceImg"
+          :hidden="!onCollect"
+          class="ma-2 rounded-sm"
+        >
+      </v-card-text>
+    </v-card>
+  </v-dialog>
 </template>
 
 <style scoped>
--- src/components/userManage/UserList.vue
+++ src/components/userManage/UserList.vue
@@ -1,20 +1,68 @@
 <script lang="ts" setup>
+
 import {ref} from "vue";
+import RecordFace from "./RecordFace.vue";
 
-const itemPerPage = ref(5)
-const items = ref<{ name: string, identity: string }[]>([])
-const itemLength = ref(100)
-const loading = ref(false)
+const data = ref<{ name: string, id: number }[]>([
+  {name: "AAA", id: 1},
+  {name: "BBB", id: 2}
+])
+const page = ref(1)
 
-const loadItems = (arg) => {
-  console.log(arg)
-}
 </script>
-
 <template>
-  <v-data-table-server
-    v-model:items-per-page="itemPerPage"
-  />
+  <v-container class="d-flex justify-center align-center">
+    <v-card
+      class="w-75 pa-3"
+    >
+      <v-card-title>
+        <record-face class="">
+          <template #activator="{active}">
+            <v-btn
+              class="d-flex justify-start"
+              prepend-icon="mdi-plus"
+              v-bind="active"
+            >
+              添加
+            </v-btn>
+          </template>
+        </record-face>
+        已登记员工
+      </v-card-title>
+      <v-card-text>
+        <v-table>
+          <thead>
+            <tr>
+              <th class="text-center">
+                Name
+              </th>
+              <th class="text-center">
+                Id
+              </th>
+              <th class="text-center">
+                Actions
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="d in data">
+              <td>{{ d.name }}</td>
+              <td>{{ d.id }}</td>
+              <td>
+                <v-btn class="bg-red">
+                  Delete
+                </v-btn>
+              </td>
+            </tr>
+          </tbody>
+        </v-table>
+        <v-pagination
+          v-model="page"
+          :length="4"
+        />
+      </v-card-text>
+    </v-card>
+  </v-container>
 </template>
 
 <style scoped>
