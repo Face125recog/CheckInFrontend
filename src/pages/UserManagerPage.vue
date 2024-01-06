26afc955dd8e02e353356ffe243dac90ac098a4e
ForzenString <964413011@qq.com>
2024-01-06 12:01:10 +0800

    :fire: 去除打卡记录

--- src/pages/UserManagerPage.vue
+++ src/pages/UserManagerPage.vue
@@ -1,10 +1,9 @@
 <template>
   <user-list />
-  <check-in-history />
+  <!--  <check-in-history />-->
 </template>
 
 <script lang="ts" setup>
 
 import UserList from "../components/userManage/UserList.vue";
-import CheckInHistory from "../components/userManage/CheckInHistory.vue";
 </script>
