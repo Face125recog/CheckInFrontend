667d35a8a9ebc71a909f6890dc04c577ee4eec9d
ForzenString <964413011@qq.com>
2024-01-07 12:36:23 +0800

    :recycle: 调整组件

--- src/pages/UserManagerPage.vue
+++ src/pages/UserManagerPage.vue
@@ -1,9 +1,15 @@
 <template>
-  <user-list />
+  <user-list
+    :manager="FrontUserManager.getInstance()"
+    :require-authorize="requireAuthorize"
+  />
   <!--  <check-in-history />-->
 </template>
 
 <script lang="ts" setup>
 
 import UserList from "../components/userManage/UserList.vue";
+import {FrontUserManager} from "../service/impls/frontUserManager.ts";
+
+const {requireAuthorize} = defineProps<{ requireAuthorize: () => Promise<string> }>()
 </script>
